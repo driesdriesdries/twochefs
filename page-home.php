c2bbd11755e25ceafbd371d0f426098e4b928679
Andries <andries@Andriess-MacBook-Pro.local>
2018-10-20 10:22:00 +0200

    updaes nav font color and gives hero a dark mask gradient

--- page-home.php
+++ page-home.php
@@ -12,13 +12,12 @@ get_header(); ?>
 	$bannerurl = get_field('banner');
 ?>
 
-<div id="banner" class="container-fluid" style="background:url('<?php echo $bannerurl['sizes']['banner'];?>'); background-position: center; background-size: cover;">
+<div id="banner" class="container-fluid" style="background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),url(<?php echo $bannerurl['sizes']['banner'];?>); background-position: center; background-size: cover; ">
 	<div class="row text-center">
 		<h1>This is the title</h1>
-		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been th</p>
 	</div>	
 </div>
-
 
 <div id="feed" class="container">
 	<div class="row">
@@ -26,31 +25,30 @@ get_header(); ?>
 			<h1>Recent Events</h1>
 		</div>
 
-<?php 
+		<?php 
 
-	$blogquery = new WP_Query(array(
-		'posts_per_page' => '6'
-	));
+			$blogquery = new WP_Query(array(
+				'posts_per_page' => '6'
+			));
 
-while($blogquery->have_posts()){
-	$blogquery->the_post(); ?>
+		while($blogquery->have_posts()){
+			$blogquery->the_post(); ?>
 
-	<?php 
-		$post_image = get_field('post_image');
-	?>
-	<div class="col-md-4">
-		<div class="item" onclick="window.location.href='<?php echo the_permalink(); ?>'" style="background:url('<?php echo $post_image['sizes']['tile'];?>'); background-position: center; background-size: cover;">
-		</div>
-		<div class="item_footer">
-			<h2><?php echo the_title(); ?></h2>
-		</div>
-	</div>
-		
-<?php }
+			<?php 
+				$post_image = get_field('post_image');
+			?>
+			<div class="col-md-4">
+				<div class="item" onclick="window.location.href='<?php echo the_permalink(); ?>'" style="background:url('<?php echo $post_image['sizes']['tile'];?>'); background-position: center; background-size: cover;">
+				</div>
+				<div class="item_footer">
+					<h2><?php echo the_title(); ?></h2>
+				</div>
+			</div>
+				
+		<?php }
 
-?>		
+		?>
 
-		
 	</div>
 </div>
 
